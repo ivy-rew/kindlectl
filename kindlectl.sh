b89a0376a60df2bc5bb1e91a4bc1000ccc400042
LordEagle <r.wermelinger@gmail.com>
2020-06-19 12:04:57 +0200

    funct to control paperwhite light

--- kindlectl.sh
+++ kindlectl.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 wifiEnabled(){ # accept no params to read state. Or 0/1 to disable/enable wifi.
-  if ! [ -z "$1" ]; then
-    lipc-set-prop com.lab126.cmd wirelessEnable $1
+  ctlKindle com.lab126.cmd wirelessEnable $1
+}
+
+light(){ # dim paperwhite light. Accept int 0-25 ...
+  ctlKindle com.lab126.powerd flIntensity $1
+}
+
+ctlKindle(){ # change or read kindle ctrl property
+  local entry="$1"
+  local key="$2"
+  local val="$3"
+  if ! [ -z "$3" ]; then
+    lipc-set-prop "$entry" "$key" "$val"
   fi
-  echo "wifi is $(lipc-get-prop com.lab126.cmd wirelessEnable)"
+  echo "$(lipc-get-prop "$entry" "$key")"
 }
 
 readProps(){ # no params or a specific entry to list it's props e.g. com.lab126.cmd
@@ -27,12 +38,25 @@ type dialog >/dev/null 2>&1 || { echo >&2 "installing dialog"; ipkg -t /opt/tmp 
 
 
 ## UI
-if [[ $1 == "wifi" ]]; then
+if [[ "$1" == "wifi" ]]; then
   dialog --yesno "enable wifi" 10 30
-  answer=$?
+  local answer=$?
   case $answer in
     0) echo "enabling";wifiEnabled 1;;
     1) echo "disabling";wifiEnabled 0;;
     255) echo "abort";;
+  esac
+fi
+
+if [[ "$1" == "light" ]]; then
+  local current=$(light)
+  dialog --inputbox "select backlight dim [0-25]" 10 30 $current 2> tmpres.txt
+  local answer=$?
+  case $answer in
+     0) dim=$(cat tmpres.txt)
+      if [[ "$dim" -gt 0 ]]; then
+        echo "light $dim";light $dim
+      fi 
+     ;;
   esac
 fi
