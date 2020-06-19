800bb966acdc19ce9c57d0e6922a89a01b1f5547
LordEagle <r.wermelinger@gmail.com>
2020-06-19 11:18:16 +0200

    dialog to ease wifi control

---
+++ README.md
@@ -0,0 +1,6 @@
+# Kindle Terminal Control
+
+## Pre-requisites
+- Jailbreak
+- USBnet or SSH Terminal session to Kindle
+- Optware for UI dialogs: https://wiki.mobileread.com/wiki/Optware
--- kindlectl.sh
+++ kindlectl.sh
@@ -11,6 +11,28 @@ readProps(){ # no params or a specific entry to list it's props e.g. com.lab126.
   if ! [ -z "$1" ]; then
     lipc-probe -v $1
   else
-    lipc-probe -l
+    lipc-probe -a
   fi
 }
+
+# deps
+if [ ! -d "/mnt/optware/opt/bin" ]; then
+  echo "optware not found!"
+  exit 1
+fi
+if [ ! -z "${PATH##*optware*}" ]; then
+  export PATH=$PATH:/mnt/optware/opt/bin
+fi
+type dialog >/dev/null 2>&1 || { echo >&2 "installing dialog"; ipkg -t /opt/tmp install dialog; }
+
+
+## UI
+if [[ $1 == "wifi" ]]; then
+  dialog --yesno "enable wifi" 10 30
+  answer=$?
+  case $answer in
+    0) echo "enabling";wifiEnabled 1;;
+    1) echo "disabling";wifiEnabled 0;;
+    255) echo "abort";;
+  esac
+fi
