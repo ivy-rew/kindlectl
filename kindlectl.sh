73aae8ed392f1d9daa1570be030438c35130b423
LordEagle <r.wermelinger@gmail.com>
2020-06-19 10:03:36 +0200

    funct to read available control properties

--- kindlectl.sh
+++ kindlectl.sh
@@ -6,3 +6,11 @@ wifiEnabled(){ # accept no params to read state. Or 0/1 to disable/enable wifi.
   fi
   echo "wifi is $(lipc-get-prop com.lab126.cmd wirelessEnable)"
 }
+
+readProps(){ # no params or a specific entry to list it's props e.g. com.lab126.cmd
+  if ! [ -z "$1" ]; then
+    lipc-probe -v $1
+  else
+    lipc-probe -l
+  fi
+}
